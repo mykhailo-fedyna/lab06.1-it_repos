5c576b3457ce59ade2789b1d3b9287eca35d0028
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-03 21:04:12 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.1(it).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestSum)
+		{
+			const int n = 5;
+			int r[n] = { 6, 10, 18, 5, 30 };
+			int expected_sum = 69;
+			int count_element = 0;
+
+			int actual_sum = Sum(r, n, count_element);
+
+			Assert::AreEqual(expected_sum, actual_sum, L"Sum calculation is incorrect.");
+			Assert::AreEqual(5, count_element, L"Element count is incorrect.");
+
+			int expected_result[n] = { 0, 0, 0, 5, 0 };
+			for (int i = 0; i < n; i++) {
+				Assert::AreEqual(expected_result[0], r[i], L"Array element is not set to zero correctly.");
+			}
+		}
+	};
+}
---
+++ lab6.1(it).cpp
@@ -0,0 +1,46 @@
+﻿// lab6.1(it).cpp
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* r, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		r[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* r, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << r[i];
+	cout << endl;
+}
+int Sum(int* r, const int size, int& count_element)
+{
+	int S = 0;
+	count_element = 0;
+	for (int i = 0; i < size; i++)
+		if ((r[i] % 6 == 0) || !(i % 5 == 0))
+		{
+			S += r[i];
+			count_element++;
+			r[i] = 0;
+		}
+	return S;
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 25;
+	int r[n];
+	int Low = 4;
+	int High = 73;
+	int count_element;
+	Create(r, n, Low, High);
+	Print(r, n);
+
+	int sum = Sum(r, n, count_element);
+	cout << "S = " << sum << endl;
+	cout << "Count of element = " << count_element << endl;
+	Print(r, n);
+	return 0;
+}
